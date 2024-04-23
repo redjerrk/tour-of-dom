29e22ee5963711ab354b0a856196046dd0c373a6
redjerrk <ocean21ratul@gmail.com>
2024-04-24 00:27:14 +0600

    updated

--- basics/scripts/script.js
+++ basics/scripts/script.js
@@ -1,1 +1,24 @@
 console.log('Hello World'); 
+
+// console.log(document);
+
+            const collectionOfLi = document.getElementsByTagName('li')
+
+            for (const li of collectionOfLi) {
+                console.log(li.innerText);
+            }
+            const h1Heading = document.getElementsByTagName('h1')
+            for (const li of h1Heading) {
+                console.log(li.innerText);
+            }
+
+            // this is how we can access HTML from JS file
+
+            const testId = document.getElementById("hello-world");
+            console.log(testId.innerText);
+            
+
+            const getClass = document.getElementsByClassName('important-class')
+            for(const className of getClass){
+                console.log(className.innerText);
+            }
