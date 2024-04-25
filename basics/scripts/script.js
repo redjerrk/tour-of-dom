88e998502db417ea932dbc827166a4e84543eb00
redjerrk <ocean21ratul@gmail.com>
2024-04-25 11:07:50 +0600

    updated

--- basics/scripts/script.js
+++ basics/scripts/script.js
@@ -1,24 +1,49 @@
-console.log('Hello World'); 
+/**
+ * We will learn 5 ways to use DOM
+ * 
+ * 
+ * Option 1 - getElementsByTagName
+ * Option 2 -getElementById
+ * Option 3 - getElementsByClassName
+ * Option 4 - querySelector --- will return the first one of the matched element
+ * Option 5 - querySelectorAll   --- will return all the matched element
+ * 
+ * 
+ * 
+ */
+
+
+console.log('Hello World');
 
 // console.log(document);
 
-            const collectionOfLi = document.getElementsByTagName('li')
+const collectionOfLi = document.getElementsByTagName('li')
 
-            for (const li of collectionOfLi) {
-                console.log(li.innerText);
-            }
-            const h1Heading = document.getElementsByTagName('h1')
-            for (const li of h1Heading) {
-                console.log(li.innerText);
-            }
+for (const li of collectionOfLi) {
+    // console.log(li.innerText);
+}
+const h1Heading = document.getElementsByTagName('h1')
+for (const li of h1Heading) {
+    console.log(li.innerText);
+}
 
-            // this is how we can access HTML from JS file
+// this is how we can access HTML from JS file
 
-            const testId = document.getElementById("hello-world");
-            console.log(testId.innerText);
-            
+const testId = document.getElementById("hello-world");
+// console.log(testId.innerText);
 
-            const getClass = document.getElementsByClassName('important-class')
-            for(const className of getClass){
-                console.log(className.innerText);
-            }
+
+const getClass = document.getElementsByClassName('important-class')
+for (const className of getClass) {
+    console.log(className.innerText);
+}
+
+const headingOne = document.getElementById('first-heading');
+headingOne.innerText = 'This is the Heading One';
+
+
+const querySelector = document.querySelectorAll('.important-class li')
+// console.log(querySelector);
+for (const li of querySelector) {
+    console.log(li.innerText);
+}
